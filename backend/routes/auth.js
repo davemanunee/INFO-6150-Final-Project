6edf98445be91d353f5eed76dd587b294405f1bd
Sunilsrinivaasneu <114025278+Sunilsrinivaasneu@users.noreply.github.com>
2022-12-08 14:49:18 -0500

    User auth check

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,7 +5,8 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
 
-//REGISTER
+//Validation of the Registration
+
 router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
@@ -43,14 +44,14 @@ router.post('/login', (req, res) => {
         if (data) {
           const accessToken = jwt.sign(
             {
-                id: user._id,
-                isAdmin: user.isAdmin,
+              id: user._id,
+              isAdmin: user.isAdmin,
             },
             process.env.JWT_SEC,
-                {expiresIn:"3d"}
-            );
+            { expiresIn: "3d" }
+          );
           const { password, ...others } = user._doc;
-          res.status(200).json({...others, accessToken});
+          res.status(200).json({ ...others, accessToken });
         } else {
           return res.status(401).json({ msg: "Username and password do not match." })
         }
