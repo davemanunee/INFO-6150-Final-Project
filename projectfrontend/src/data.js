321b6991b16b3a36e79a662f56a1f5e831fd641d
davemanunee <dave.man@northeastern.edu>
2022-12-08 12:34:20 -0500

    Added pages and added responsiveness, created necessary changes in the code .

---
+++ projectfrontend/src/data.js
@@ -0,0 +1,84 @@
+
+export const sliderItems = [
+    {
+      id: 1,
+      img: '/assets/women-carousel.jpg',
+      title: "SUMMER SALE FOR WOMEN",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+      cat:"women"
+      
+    },
+    {
+      id: 2,
+      img: "/assets/men-carousel.jpg",
+      title: "FALL COLLECTION FOR MEN",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+      cat:"men"
+    },
+    {
+      id: 3,
+      img: "/assets/home-carousel.jpg",
+      title: "HOME DECOR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+      cat:"homedecor"
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "/assets/women-icon.jpg",
+      title: "SHOP WOMEN !!",
+      cat:"women"
+    },
+    {
+      id: 2,
+      img:"/assets/men-icon.jpg",
+      title: "SHOP MEN !!",
+      cat:"men"
+    },
+    {
+      id: 3,
+      img:"/assets/home-icon.jpg",      
+      title: "SHOP HOME-DECOR",
+      cat:"homedecor"
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"/assets/woman-formal.png",
+    },
+    {
+      id:2,
+      img:"/assets/men-casual.png"
+    },
+    {
+      id:3,
+      img:"/assets/women-casual.png"
+    },
+    {
+      id:4,
+      img:"/assets/men-formal.png"
+    },
+    {
+      id:5,
+      img:"/assets/women-heels.png"
+    },
+    {
+      id:6,
+      img:"/assets/home-decor1.png"
+    },
+    {
+      id:7,
+      img:"/assets/women-accessories.png"
+    },
+    {
+      id:8,
+      img:"/assets/home-decor2.png"
+    },
+  ]
